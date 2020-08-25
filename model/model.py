a858e7164935eec86c9fb23103b93087a070377d
Drew Linsley <drewlinsley@gmail.com>
2020-08-25 12:49:50 -0400

    Iterating.

--- model/model.py
+++ model/model.py
@@ -230,39 +230,39 @@ def build_model_fn(model, num_classes, num_train_examples):
                   tf.argmax(labels['labels'], 1), tf.argmax(logits_sup, axis=1))
               label_acc = tf.reduce_mean(tf.cast(label_acc, tf.float32))
               step = tf.train.get_or_create_global_step()
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'train_bottomup_loss',
                   bu_loss,
                   )  # step=step)
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'train_topdown_loss',
                   td_loss,
                   )  # step=step)
               
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'train_bottomup_acc',
                   contrast_bu_acc,
                   )  # step=step)
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'train_topdown_acc',
                   contrast_td_acc,
                   )  # step=step)
               
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'train_label_accuracy',
                   label_acc,
                   )  # step=step)
               
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'contrast_bu_entropy',
                   entropy_bu_con,
                   )  # step=step)
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'contrast_td_entropy',
                   entropy_td_con,
                   )  # step=step)
               
-              tf.summary.scalar(
+              tf2.summary.scalar(
                   'learning_rate', learning_rate,
                   )  # step=step)
 
@@ -273,18 +273,18 @@ def build_model_fn(model, num_classes, num_train_examples):
               print(viz_features)
               print("Reconstruction")
               print(reconstruction)
-              # tf.summary.image(
-              #     'Images',
-              #     tf.cast(target_images, tf.float32),
-              #     )  # step=step)
-              # tf.summary.image(
-              #     'Transformed_images',
-              #     tf.cast(viz_features, tf.float32),
-              #     )  # step=step)
-              # tf.summary.image(
-              #     'Reconstructed_images',
-              #     tf.cast(reconstruction, tf.float32),
-              #     )  # step=step)
+              tf2.summary.image(
+                  'Images',
+                  tf.cast(target_images, tf.float32),
+                  step=step)
+              tf2.summary.image(
+                  'Transformed images',
+                  tf.cast(viz_features, tf.float32),
+                  step=step)
+              tf2.summary.image(
+                  'Reconstructed images',
+                  tf.cast(reconstruction, tf.float32),
+                  step=step)
 
       optimizer = model_util.get_optimizer(learning_rate)
       control_deps = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
