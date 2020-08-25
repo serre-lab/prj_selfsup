747e778e81bac0dd6a4fe3dc0b3dfd778a61c4c0
Drew Linsley <drewlinsley@gmail.com>
2020-08-25 12:51:44 -0400

    Iterating.

--- run_imagenet.py
+++ run_imagenet.py
@@ -593,6 +593,6 @@ def main(argv):
 
 
 if __name__ == '__main__':
-  # tf.disable_v2_behavior()  # Disable eager mode when running with TF2.
+  tf.disable_v2_behavior()  # Disable eager mode when running with TF2.
   # tf.disable_eager_execution()
   app.run(main)
