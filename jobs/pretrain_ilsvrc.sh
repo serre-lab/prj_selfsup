7edb46f9658c17e9780fe0f5157df2e0eb5a05a4
Drew Linsley <drewlinsley@gmail.com>
2020-08-27 21:37:29 +0000

    Removed deletions from job scripts.

--- jobs/pretrain_ilsvrc.sh
+++ jobs/pretrain_ilsvrc.sh
@@ -2,7 +2,7 @@
 
 train_mode=pretrain
 
-train_batch_size=1024
+train_batch_size=4096
 train_epochs=300 
 temperature=0.1
 
@@ -35,12 +35,14 @@ use_neptune=False
 experiment_name="BU_{bu_loss}_TD_{td_loss}_R${encoder_depth}_lr${learning_rate}_T${temperature}"
 echo "Deleting gs://serrelab/prj-selfsup/${experiment_name} and tmp files"
 echo gs://serrelab/prj-selfsup/${experiment_name} > current_job.txt
-gsutil  rm -r gs://serrelab/prj-selfsup/${experiment_name}
-sudo rm -rf /tmp/*
+
+# gsutil  rm -r gs://serrelab/prj-selfsup/${experiment_name}
+# sudo rm -rf /tmp/*
 
 use_tpu=True
-export TPU_NAME='prj-selfsup-tpu'
+# export TPU_NAME='prj-selfsup-tpu'
 # export TPU_NAME='prj-selfsup-tpu-preempt0'
+export TPU_NAME='prj-selfsup-v2-22'
 export STORAGE_BUCKET='gs://serrelab/prj-selfsup'
 DATA_DIR=gs://imagenet_data/train/
 MODEL_DIR=$STORAGE_BUCKET/$experiment_name
