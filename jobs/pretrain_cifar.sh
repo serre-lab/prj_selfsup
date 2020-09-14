94e63db064cbfabfb58285764f536d15bf961802
aimen zerroug <aimen.zerroug@cnrs.fr>
2020-09-14 09:34:16 -0400

    sh config

--- jobs/pretrain_cifar.sh
+++ jobs/pretrain_cifar.sh
@@ -19,26 +19,11 @@ resnet_depth=18
 use_blur=True 
 color_jitter_strength=0.5 
 
-experiment_name="cifar_test"
+experiment_name="cifar_moco_test"
 use_neptune=True
 
-model_dir="/home/aimen/projects/prj_selfsup_test_3" 
+model_dir="/home/aimen/projects/prj_selfsup_moco" 
 use_tpu=False
-
-use_td_loss=True #False
-use_bu_loss=True
-
-td_loss=attractive_repulsive #'attractive_repulsive'
-# 'attractive' L2 reconstruction
-# 'attractive_repulsive' our idea
-
-
-bu_loss=attractive_repulsive #'attractive_repulsive'
-# 'attractive' cosine distance
-# 'attractive_repulsive' simclr
-
-td_loss_weight=1.0
-bu_loss_weight=1.0
 
 GPU_IDX=0
 
@@ -51,9 +36,6 @@ CUDA_VISIBLE_DEVICES=GPU_IDX python run.py \
     --learning_rate=$learning_rate --weight_decay=$weight_decay --temperature=$temperature \
     --dataset=$dataset --image_size=$image_size --eval_split=$eval_split --resnet_depth=$resnet_depth \
     --use_blur=$use_blur --color_jitter_strength=$color_jitter_strength \
-    --use_td_loss=$use_td_loss --use_bu_loss=$use_bu_loss \
-    --td_loss=$td_loss --bu_loss=$bu_loss \
-    --td_loss_weight=$td_loss_weight --bu_loss_weight=$bu_loss_weight \
     --model_dir=$model_dir --use_tpu=$use_tpu \
     --experiment_name=$experiment_name --use_neptune=$use_neptune  
 
